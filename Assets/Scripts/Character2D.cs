45a0a4f55e7a85765b50b8f5c9aed5fbf8e08edf
jfcarocota <jfcarocota@gmail.com>
2020-01-08 16:25:33 -0700

    basic movemente and flipx

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    Animator anim;
+    SpriteRenderer spr;
+    [SerializeField, Range(0.1f, 20f)]
+    float moveSpeed = 2f;
+
+    void Awake() 
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();  
+    }
+
+    void Update() 
+    {
+        transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);   
+        spr.flipX = Flip; 
+    }
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+   bool Flip
+   {
+       get => Axis.x > 0f ? false : Axis.x < 0f ? true : spr.flipX;
+   }
+}
