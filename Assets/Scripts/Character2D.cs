e4f3535fe2da76b7c569ef016956d9aa182a2d0d
jfcarocota <jfcarocota@gmail.com>
2020-01-09 20:36:41 -0700

    jump and rigidbody2d

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -6,13 +6,24 @@ public class Character2D : MonoBehaviour
 {
     Animator anim;
     SpriteRenderer spr;
+    Rigidbody2D rb2D;
     [SerializeField, Range(0.1f, 20f)]
     float moveSpeed = 2f;
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 20f)]
+    float rayDistance = 5f;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField, Range(0.1f, 20f)]
+    float jumpForce = 7f;
 
     void Awake() 
     {
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();  
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update() 
@@ -21,9 +32,22 @@ public class Character2D : MonoBehaviour
         spr.flipX = Flip; 
     }
 
+    void FixedUpdate() 
+    {
+        if(Grounding)
+        {
+            if(JumpButton)
+            {
+                rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                anim.SetTrigger("jump");
+            }
+        }
+    }
+
     void LateUpdate()
     {
         anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+        anim.SetBool("grounding", Grounding);
     }
 
     Vector2 Axis
@@ -31,8 +55,24 @@ public class Character2D : MonoBehaviour
         get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
-   bool Flip
-   {
+    bool Flip
+    {
        get => Axis.x > 0f ? false : Axis.x < 0f ? true : spr.flipX;
-   }
+    }
+
+    bool Grounding
+    {
+        get => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+    }
+
+    bool JumpButton
+    {
+        get => Input.GetButtonDown("Jump");
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+       Gizmos.color = rayColor;
+       Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
+    }
 }
