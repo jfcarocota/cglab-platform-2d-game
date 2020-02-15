9170e1a758b4bd92646756a7811e57439c182fbb
jfcarocota <jfcarocota@gmail.com>
2020-02-15 13:31:12 -0700

    basic animation controller

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,25 +9,53 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float timeLimit;
     float timer = 0;
+    float timerDelay = 0;
+    [SerializeField]
+    float delay;
     [SerializeField]
     Vector2 dir = Vector2.right;
 
     SpriteRenderer spr;
 
+    Animator anim;
+
     void Awake() 
     {
-        spr = GetComponent<SpriteRenderer>();    
+        spr = GetComponent<SpriteRenderer>();   
+        anim = GetComponent<Animator>(); 
     }
 
     void Update()
     {
-        transform.Translate(dir * moveSpeed * Time.deltaTime);
+        /*transform.Translate(dir * moveSpeed * Time.deltaTime);
         timer += Time.deltaTime;
         if(timer >= timeLimit)
         {
             timer = 0;
             spr.flipX = FlipSprite;
             dir.x = dir.x > 0 ? -1 : 1;
+        }*/
+
+        if(timer < timeLimit)
+        {
+            transform.Translate(dir * moveSpeed * Time.deltaTime);
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            if(timerDelay == 0)
+            {
+                anim.SetTrigger("idle");
+            }
+            timerDelay += Time.deltaTime;
+            if(timerDelay >= delay)
+            {
+                spr.flipX = FlipSprite;
+                dir.x = dir.x > 0 ? -1 : 1;
+                timer = 0;
+                timerDelay = 0;
+                anim.SetTrigger("patrol");
+            }
         }
     }
 
